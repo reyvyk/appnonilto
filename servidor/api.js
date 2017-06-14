e1bb35f120474548fd9e5153ac025db7a312e876
Iosley Carlos Silva <iosley@outlook.com>
2017-06-14 00:24:06 -0300

    Adicionado lado servidor para requisitar os dados

---
+++ servidor/api.js
@@ -0,0 +1,36 @@
+const Express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+
+const app = Express()
+
+const port = process.env.PORT || 3000
+
+let myDB;
+
+app.get('/', (req, res) => {
+  res.send('Ta funcionando KRALHO!!!')
+})
+
+app.get('/noticias', (req, res, next) => {
+  let collection = myDB.collection('pages');
+
+  collection.find({})
+    .toArray((err, docs) => {
+      // Se der erro passa o erro pra frente
+      if (err) return next(err)
+
+      // envia os a resposta para o usuário
+      res.send(docs)
+    })
+})
+
+
+MongoClient.connect(url, (err, db) => {
+  if (err) return console.log('Deu ruim!!!')
+
+  myDB = db
+
+  app.listen(port, () => {
+    console.log('Ta funcionando KRALHO!!!')
+  })
+}
--- utils.js
+++
@@ -1,7 +0,0 @@
-
-module.exports.inserir = function insert(db, collectionName, dados, calback) {
-
-	let collection = db.collection(collectionName);	
-	collection.insert(dados, calback);
-
-};
