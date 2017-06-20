18bb441604fb220cec4457592954222d36a16c80
Iosley Carlos Silva <iosley@outlook.com>
2017-06-19 21:44:00 -0300

    Adicionado cards com noticias

---
+++ src/components/card-noticia.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <figure>
+      <img src="{{ image }}">
+    </figure>
+    <title>{{ title }}</title>
+    <p>{{ chamada }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'image',
+    'title',
+    'chamada'
+  ]
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
-    <table-api></table-api>
+    <template v-if="noticias">
+      <h1>Noticias</h1>
+      <card-noticia v-for="noticia in noticias" :image="noticia.Image" :title="noticia.Title" :chamada="noticia.Chamada"></card-noticia>
+    </template>
+    <h1 v-else>Sem Noticias</h1>
   </div>
 </template>
 
 <script>
-import tableapi from './../components/apitec.vue'
+import request from 'request'
+import cardNoticia from './../components/card-noticia.vue'
+
 export default {
   name: 'app',
   data () {
-    return {}
+    return {
+      noticias: []
+    }
+  },
+  mounted () {
+    request('http://127.0.0.1:3000/noticias', (error, response, body) => {
+      if (error) return console.log(error), alert('erro')
+
+      this.noticias = typeof body !== 'object'
+        ? JSON.parse(body)
+        : body
+    });
   },
   components: {
-    'table-api' : tableapi,
+    cardNoticia
   }
 }
 </script>
